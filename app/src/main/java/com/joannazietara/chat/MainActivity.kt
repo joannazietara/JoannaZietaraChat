837f2cc884562ee25cf043ff895fa1840a8cb6da
joannazietara <joanna.zietara@microsoft.wsei.edu.pl>
2019-03-15 02:01:14 +0100

    Ogólny zarys aplikacji. Widok braku konwersacji oraz dymki odpowiedzi i pytań

--- app/src/main/java/com/joannazietara/chat/MainActivity.kt
+++ app/src/main/java/com/joannazietara/chat/MainActivity.kt
@@ -2,11 +2,43 @@ package com.joannazietara.chat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
 
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottomNavigation.setOnNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.navigation_rooms -> {
+                tvChatAnswer.visibility = View.INVISIBLE
+                tvUserQuestion.visibility = View.INVISIBLE
+                tvNoConversationCaption.visibility = View.VISIBLE
+                ivNoConversationImage.visibility = View.VISIBLE
+                return true
+            }
+            R.id.navigation_where -> {
+                tvChatAnswer.visibility = View.INVISIBLE
+                tvUserQuestion.visibility = View.INVISIBLE
+                tvNoConversationCaption.visibility = View.VISIBLE
+                ivNoConversationImage.visibility = View.VISIBLE
+                return true
+            }
+            R.id.navigation_other -> {
+                tvChatAnswer.visibility = View.VISIBLE
+                tvUserQuestion.visibility = View.VISIBLE
+                tvNoConversationCaption.visibility = View.INVISIBLE
+                ivNoConversationImage.visibility = View.INVISIBLE
+                return true
+            }
+        }
+        return false
     }
 }
